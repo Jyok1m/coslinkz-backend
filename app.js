7a308e8dd6496e67370fef5dab225ca384ddea0f
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-04-30 18:50:54 +0200

    created auth route file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth.routes");
 
 var app = express();
 
@@ -19,5 +20,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 module.exports = app;
