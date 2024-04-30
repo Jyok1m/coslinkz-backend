2b5ac88f2a0bf64c5a41bea977aa2709fa7d09e6
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-04-30 18:48:41 +0200

    created user model

---
+++ db/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose").default;
+
+const rightSchema = new mongoose.Schema({
+	type: { type: String, required: true, enum: ["user", "admin"] },
+	registration: { type: String, required: false, default: "pending", enum: ["pending", "confirmed"] },
+	confirmationCode: { type: String, required: false, default: "" },
+});
+
+const userSchema = new mongoose.Schema(
+	{
+		username: { type: String, required: true },
+		email: { type: String, required: true },
+		password: { type: String, required: true },
+		avatar: { type: String, required: false, default: "" },
+		rights: rightSchema,
+		status: { type: String, required: false, default: "offline", enum: ["online", "offline"] },
+		friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "users", required: false }] || [],
+		// Stripe
+		customerId: { type: String, required: false, default: "" },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const userModel = mongoose.models.users || new mongoose.model("users", userSchema);
+
+module.exports = userModel;
