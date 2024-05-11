2fd872ce5ad5ed749c4404d9c829f29fcd7ca89e
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-05-11 13:38:10 +0200

    friendship creation

--- modules/user.js
+++ modules/user.js
@@ -1,4 +1,9 @@
 const User = require("../db/models/User");
+const Friendship = require("../db/models/Friendship");
+
+/* ---------------------------------------------------------------- */
+/*                           Handle avatar                          */
+/* ---------------------------------------------------------------- */
 
 async function updateAvatar(userId = "", uri = "") {
 	try {
@@ -13,4 +18,64 @@ async function updateAvatar(userId = "", uri = "") {
 	}
 }
 
-module.exports = { updateAvatar };
+/* ---------------------------------------------------------------- */
+/*                        Handle friend list                        */
+/* ---------------------------------------------------------------- */
+
+async function updateFriendList(userId = "", username = "", requestType = "") {
+	try {
+		if (username) {
+			const target = await User.findOne({ username: { $regex: new RegExp(username, "i") } }).select("_id");
+			if (target) {
+				const friendship = await Friendship.findOne({
+					$or: [
+						{ sender: userId, receiver: target._id },
+						{ sender: target._id, receiver: userId },
+					],
+				});
+
+				if (friendship) {
+					const { status } = friendship;
+					if (status === "locked") {
+						return { success: false, error: "Operation forbidden. Please contact the support team" };
+					} else if (requestType === "create") {
+						if (status === "pending") {
+							return { success: false, error: "Friend request already awaiting confirmation" };
+						} else if (status === "confirmed") {
+							return { success: false, error: "Friend already in your list" };
+						} else {
+							await Friendship.findByIdAndUpdate(friendship._id, { sender: userId, receiver: target._id, status: "pending" });
+							return { success: true, message: "Friendship request successfully sent" };
+						}
+					} else {
+						if (status === "cancelled") {
+							return { success: false, error: "Person already cancelled" };
+						} else {
+							if (status === "confirmed") {
+								await User.updateMany({ $or: [{ _id: userId }, { _id: target._id }] }, { $pull: { friendships: friendship._id } });
+							}
+							await Friendship.findByIdAndUpdate(friendship._id, { sender: userId, receiver: target._id, status: "cancelled" });
+							return { success: true, message: "Friendship successfully cancelled" };
+						}
+					}
+				} else {
+					if (requestType === "create") {
+						const newFriendship = await new Friendship({ sender: userId, receiver: target._id, status: "pending" });
+						await newFriendship.save();
+						return { success: true, message: "Friendship request successfully sent" };
+					} else {
+						return { success: false, error: "No friendship found" };
+					}
+				}
+			} else {
+				return { success: false, error: "Target user not found" };
+			}
+		} else {
+			return { success: false, error: "Missing username" };
+		}
+	} catch (e) {
+		return { success: false, error: e.message };
+	}
+}
+
+module.exports = { updateAvatar, updateFriendList };
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-const { updateAvatar } = require("../modules/user");
+const { updateAvatar, updateFriendList } = require("../modules/user");
 
 // Middleware
 const { checkAccess } = require("../middlewares/auth");
@@ -24,7 +24,29 @@ router.post("/avatar", checkAccess, upload.single("avatar"), async (req, res) =>
 			return res.json({ success: false, error: update.error });
 		}
 
-		res.json({ result: true, message: "Avatar ajouté avec succès !", avatar: upload.uri });
+		res.json({ ...upload, message: update.message });
+	} catch (e) {
+		console.error(e); // Log the error for debugging
+		res.json({ success: false, error: e.message });
+	}
+});
+
+router.put("/friendship", checkAccess, async (req, res) => {
+	try {
+		const { userId } = req;
+		const { request, username } = req.query;
+		const requestTypes = ["create", "cancel"];
+
+		if (typeof request === "string" && requestTypes.includes(request)) {
+			const update = await updateFriendList(userId, username, request);
+			if (update.success) {
+				return res.json({ ...update });
+			} else {
+				return res.json({ success: false, error: update.error });
+			}
+		} else {
+			return res.json({ success: false, error: "Invalid request" });
+		}
 	} catch (e) {
 		console.error(e); // Log the error for debugging
 		res.json({ success: false, error: e.message });
