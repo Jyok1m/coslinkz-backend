7d797fd56cd040e002a5a7c0cd71ce43e4eea992
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-07-02 09:58:01 +0200

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
-var cors = require("cors");
+var cors = require("cors"); //
 
 var indexRouter = require("./routes/index");
 var authRouter = require("./routes/auth");
